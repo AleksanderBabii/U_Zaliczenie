bd0c53adaee8de27207562a4352e9a49772f257a
DESKTOP-UIHOCEQ\sasha <a.anchypolivski@gmail.com>
2022-06-01 13:38:41 +0200

    Final version of the prototype

--- Assets/Scripts/Player/Shrine/PickUp.cs
+++ Assets/Scripts/Player/Shrine/PickUp.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    public GameObject hitEffect;
     private void OnTriggerEnter(Collider other)
     {
         PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
@@ -13,5 +14,10 @@ public class PickUp : MonoBehaviour
             playerInventory.ShrineCollected();
             gameObject.SetActive(false);
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 4f);
     }
 }
