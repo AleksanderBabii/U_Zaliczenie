25a403a66cdc9c06be0b5d1898ac722c4bfe2fed
Kongoseki <82161795+AleksanderBabii@users.noreply.github.com>
2025-01-09 11:10:28 +0100

    game version 0.0.1 is released on itch.io

--- Assets/Scripts/Menu/GameOverMenu.cs
+++ Assets/Scripts/Menu/GameOverMenu.cs
@@ -3,15 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
-
-    private void Update()
+    private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("GamePrototype");
     }
     public void QuitGame()
     {
